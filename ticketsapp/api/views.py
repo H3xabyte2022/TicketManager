2ca84bd8136882e6212a4f808b249caa574d9a7b
Julian Ramirez <julianramirezfelipe@gmail.com>
2024-05-28 21:04:25 -0500

    More necesary views

--- ticketsapp/api/views.py
+++ ticketsapp/api/views.py
@@ -4,6 +4,9 @@ from rest_framework.permissions import IsAuthenticated
 from ..models import Ticket, Image
 from .serializers import TicketSerializer, ImageSerializer
 from ..tasks import upload_image_to_cloudinary
+from django.core.exceptions import PermissionDenied
+
+
 
 class TicketCreateView(generics.CreateAPIView):
     queryset = Ticket.objects.all()
@@ -36,3 +39,35 @@ class ImageUploadView(generics.CreateAPIView):
 
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+    
+    
+    
+class TicketListView(generics.ListAPIView):
+    serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        queryset = Ticket.objects.filter(user=self.request.user)
+        status = self.request.query_params.get('status')
+        start_date = self.request.query_params.get('start_date')
+        end_date = self.request.query_params.get('end_date')
+        
+        if status:
+            queryset = queryset.filter(status=status)
+        if start_date and end_date:
+            queryset = queryset.filter(created_at__range=[start_date, end_date])
+        
+        return queryset
+
+
+
+class TicketDetailView(generics.RetrieveAPIView):
+    queryset = Ticket.objects.all()
+    serializer_class = TicketSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        ticket = super().get_object()
+        if ticket.user != self.request.user:
+            raise PermissionDenied('You do not have permission to view this ticket')
+        return ticket    

ce347ce4c3543bf2554965450aa65a83ac1ddbd1
Julian Ramirez <julianramirezfelipe@gmail.com>
2024-05-28 20:51:03 -0500

    Basic Ticket and IMG model

--- ticketsapp/models.py
+++ ticketsapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+
+"""This is the main ticket model"""
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    num_images = models.PositiveIntegerField()
+    status = models.CharField(max_length=20, choices=[('pending', 'Pending'), ('completed', 'Completed')], default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+"""This is an image model used for tracking Image information"""
+class Image(models.Model):
+    ticket = models.ForeignKey(Ticket, related_name='images', on_delete=models.CASCADE)
+    image_url = models.URLField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)

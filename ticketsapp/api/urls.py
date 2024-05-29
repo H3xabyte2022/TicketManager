db3287a75d9373a700f49cac920eeea34d9ea35e
Julian Ramirez <julianramirezfelipe@gmail.com>
2024-05-28 21:05:02 -0500

    More urls

--- ticketsapp/api/urls.py
+++ ticketsapp/api/urls.py
@@ -4,5 +4,6 @@ from .views import TicketCreateView, ImageUploadView, TicketListView, TicketDeta
 urlpatterns = [
     path('tickets/', TicketCreateView.as_view(), name='ticket-create'),
     path('upload-image/', ImageUploadView.as_view(), name='image-upload'),
-    
+    path('my-tickets/', TicketListView.as_view(), name='ticket-list'),
+    path('tickets/<int:pk>/', TicketDetailView.as_view(), name='ticket-detail'),
 ]

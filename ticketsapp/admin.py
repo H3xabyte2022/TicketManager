f395b9246061b2a50cd21e65366bb3e11491333d
Julian Ramirez <julianramirezfelipe@gmail.com>
2024-05-28 21:12:21 -0500

    Admin addded

--- ticketsapp/admin.py
+++ ticketsapp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Ticket, Image
 
-# Register your models here.
+
+
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('user', 'num_images', 'status', 'created_at', 'updated_at')
+    readonly_fields = ('user', 'num_images', 'status', 'created_at', 'updated_at')
+
+
+
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('ticket', 'image_url', 'uploaded_at')
+    readonly_fields = ('ticket', 'image_url', 'uploaded_at')
